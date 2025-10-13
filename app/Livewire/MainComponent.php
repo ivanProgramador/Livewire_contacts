0635f77b2e2fe449bfd153be96cd9218796600b4
Ivan Rodrigues <ivan-guita@hotmail.com>
2025-10-13 11:18:56 -0300

    base de dados conectada,tabela criada, titulo estabelecido

--- app/Livewire/MainComponent.php
+++ app/Livewire/MainComponent.php
@@ -2,10 +2,12 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
 class MainComponent extends Component
 {
+    #[Title('Livewire contacts')]
     public function render()
     {
         return view('livewire.main-component');
